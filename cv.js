3283058773d602831df8183bb9c1dc9786e52549
jsoletta <102762625+jsoletta@users.noreply.github.com>
2023-01-24 16:13:02 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,8 @@
+function funcionMostrar() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
